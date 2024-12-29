babce011fb002655225ba52d44f359240a86f3eb
defaultuser0 <axolotlslmao@gmail.com>
2024-12-29 15:05:52 -0600

    ads

---
+++ CapuchinTemplate/ModInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CapuchinTemplate
+{
+    public class ModInfo
+    {
+        public const string Name = "CapuchinTemplate";
+        public const string Description = "Capuchin Template";
+        public const string Author = "CapuchinTemplateAuthor";
+        public const string Version = "1.0.0";
+    }
+}
